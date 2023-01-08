5f3850e2047e265d6d1413fdab60b18fc80d52e0
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-08 14:07:53 +0900

    스프링 시큐리티 2부: 스큐리티 설정 커스터마이징

--- src/main/java/me/whiteship/springbootsecurity2/account/AccountService.java
+++ src/main/java/me/whiteship/springbootsecurity2/account/AccountService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -18,10 +19,13 @@ public class AccountService implements UserDetailsService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Account createAccount(String username, String password) {
         Account account = new Account();
         account.setUsername(username);
-        account.setPassword(password);
+        account.setPassword(passwordEncoder.encode(password));
         return accountRepository.save(account);
     }
 
--- src/main/java/me/whiteship/springbootsecurity2/config/SecurityConfig.java
+++ src/main/java/me/whiteship/springbootsecurity2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,6 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
